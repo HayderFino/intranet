aee7b0293c9a0da35d9830eb592f663c869f0461
Hayder Fino <hafssuarez@gmail.com>
2026-02-28 16:14:59 -0500

    Delete a file.

--- practicas/test_create.js
+++
@@ -1,9 +0,0 @@
-const SgiModel = require('./src/models/sgiModel');
-
-try {
-    console.log('Testing create in mejora...');
-    const id = SgiModel.create('mejora', 'Test Document', 'Procedimientos', '../../data/sgi/test.pdf');
-    console.log('Created ID:', id);
-} catch (e) {
-    console.error('ERROR DETECTED:', e);
-}
--- practicas/test_model.js
+++
@@ -1,6 +0,0 @@
-const NewsModel = require('./src/models/newsModel');
-const news = NewsModel.getAll();
-console.log(`Loaded ${news.length} news items.`);
-if (news.length > 0) {
-    console.log(`First item: ${JSON.stringify(news[0], null, 2)}`);
-}
