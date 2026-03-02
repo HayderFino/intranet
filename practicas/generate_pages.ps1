b3d66bfc6daec521ea6bccb5c6c045bcb00413f3
Hayder Fino <hafssuarez@gmail.com>
2026-03-02 16:09:21 -0500

    commit

--- practicas/generate_pages.ps1
+++ practicas/generate_pages.ps1
@@ -108,7 +108,7 @@ $template = @"
                                 <a href="#">Planes</a>
                                 <a href="#">Convocatorias</a>
                                 <a href="#">Estudios T&eacute;cnicos</a>
-                                <a href="#">Provisi&oacute;n de empleos</a>
+                                <a href="{{REL}}header_menu/cas/provision-empleos.html">Provisi&oacute;n de empleos</a>
                                 <a href="https://cas.delfineco.com/gdpagos_cas/" target="_blank">Desprendibles de Nomina</a>
                             </div>
                         </div>
@@ -216,3 +216,4 @@ $html_meci = $template.Replace('{{REL}}', '').Replace('{{TITLE}}', 'MECI').Repla
 [System.IO.File]::WriteAllText("$(Get-Location)/meci.html", $html_meci)
 
 Write-Host "Portal re-generado con entidades HTML."
+
